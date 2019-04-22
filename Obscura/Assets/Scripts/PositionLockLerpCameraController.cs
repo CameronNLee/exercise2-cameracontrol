3a4319b38cd9528bfdee69015b4b60be8a34bc46
cmnlee <cmnlee@ucdavis.edu>
2019-04-22 15:12:18 -0700

    Setting up stage3 script

---
+++ Obscura/Assets/Scripts/PositionLockLerpCameraController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Obscura
+{
+    public class PositionLockLerpCameraController : AbstractCameraController
+    {
+        [SerializeField] public float LerpDuration;
+        
+        private Camera ManagedCamera;
+        private LineRenderer CameraLineRenderer;
+
+        private void Awake()
+        {
+            this.ManagedCamera = this.gameObject.GetComponent<Camera>();
+            this.CameraLineRenderer = this.gameObject.GetComponent<LineRenderer>();
+        }
+
+        private void LateUpdate()
+        {
+            var targetPosition = this.Target.transform.position;
+            var cameraPosition = this.ManagedCamera.transform.position;
+        }
+
+        public override void DrawCameraLogic()
+        {
+            // fill in
+        }
+    }
+}
