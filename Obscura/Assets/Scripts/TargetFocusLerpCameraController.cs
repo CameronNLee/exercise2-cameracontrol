71029d5ff86cd5f10615908c7e7c3be2ca0dae89
cmnlee <cmnlee@ucdavis.edu>
2019-04-23 23:52:04 -0700

    Implemented stage 4

--- Obscura/Assets/Scripts/TargetFocusLerpCameraController.cs
+++ Obscura/Assets/Scripts/TargetFocusLerpCameraController.cs
@@ -23,7 +23,7 @@ namespace Obscura
 
         private bool isMoving;
         
-        public void Awake()
+        private void Awake()
         {
             this.ManagedCamera = this.gameObject.GetComponent<Camera>();
             this.CameraLineRenderer = this.gameObject.GetComponent<LineRenderer>();
@@ -32,7 +32,7 @@ namespace Obscura
             StartTime = Time.time;
             ElapsedTime = 0.0f;
             
-            StartPosition = this.ManagedCamera.transform.position;
+            StartPosition = this.Target.transform.position;
             EndPosition = StartPosition;
         }
 
@@ -42,10 +42,80 @@ namespace Obscura
             {
                 // Moving, so use Lerp scheme
                 var cameraPosition = this.ManagedCamera.transform.position;
+                var targetPosition = this.Target.transform.position;
+                // The conditional directly below allows us to only set
+                // StartPosition once per "movement", which is what we want.
+                if (!isMoving)
+                {
+                    isMoving = true;
+                    StartPosition = this.ManagedCamera.transform.position;
+                }
+                
+                // Update EndPosition to where the player Target is
+                // PLUS the direction the player is heading towards
+                // by adding the player's speed.
+                var movementDirection = PlayerObject.GetMovementDirection();
+                var currentSpeed = PlayerObject.GetCurrentSpeed();
+                var xComponent = (currentSpeed / 10.0f) * movementDirection.x;
+                var yComponent = (currentSpeed / 10.0f) * movementDirection.y;
+                
+                EndPosition = new Vector3(targetPosition.x + xComponent, targetPosition.y + yComponent, cameraPosition.z);
+
+                if (ElapsedTime >= LerpDuration * 0.85f)
+                {
+                    // Update the camera according to threshold.
+                    ElapsedTime = LerpDuration * 0.85f;
+                }
+                else
+                {
+                    ElapsedTime = (Time.time - StartTime);
+                }
+                
+                // Update the camera according to Linear Interpolation scheme.
+                cameraPosition = Vector3.Lerp(StartPosition, EndPosition, (ElapsedTime + LeadSpeed / LerpDuration));
+                cameraPosition.z = this.ManagedCamera.transform.position.z;
+
+                // Finally, truly update the camera position.
+                this.ManagedCamera.transform.position = cameraPosition;
             }
             else
             {
                 // No longer moving. Still use Lerp scheme so camera aligns with player position.
+                if (!cameraHasCaughtUpToPlayer())
+                {
+                    if (isMoving)
+                    {
+                        isMoving = false;
+                        StartTime = Time.time;
+                        StartPosition = this.ManagedCamera.transform.position;
+                    }
+
+                    ElapsedTime = (Time.time - StartTime);
+                    EndPosition = this.Target.transform.position;
+                    // Keep updating the camera according to Linear interpolation
+                    // until the camera catches up to the player who is no longer moving.
+                    var cameraPosition = Vector3.Lerp(StartPosition, EndPosition, (ElapsedTime / LerpDuration));
+                    cameraPosition.z = this.ManagedCamera.transform.position.z;
+                    this.ManagedCamera.transform.position = cameraPosition;
+
+                }
+                else
+                {
+                    // Camera position is now equal to player position, so reset these values.
+                    ElapsedTime = 0.0f;
+                    StartTime = Time.time;
+                    StartPosition = EndPosition;
+                }
+            }
+
+            if (this.DrawLogic)
+            {
+                this.CameraLineRenderer.enabled = true;
+                this.DrawCameraLogic();
+            }
+            else
+            {
+                this.CameraLineRenderer.enabled = false;
             }
         }
 
@@ -72,6 +142,12 @@ namespace Obscura
             // building vertical line of the 5x5 cross
             this.CameraLineRenderer.SetPosition(3, verticalPointUp);
             this.CameraLineRenderer.SetPosition(4, verticalPointDown);
+        }
+        
+        private bool cameraHasCaughtUpToPlayer()
+        {
+            return (this.ManagedCamera.transform.position.x == this.Target.transform.position.x
+                    && this.ManagedCamera.transform.position.y == this.Target.transform.position.y);
         }
     }
 }
